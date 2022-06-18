8513d9acee7781c5b90dc7540ea6a73d8080cca1
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2022-06-18 15:16:32 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function login() {
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
 
-    if(email == "admin@gmail.com" && senha == "zxc") {
+    if(email == "admin@gmail.com" && senha == "7894561230101") {
         location.href = '../page/home.html';
     } else{
         alert('Senha ou Email incorreto');
